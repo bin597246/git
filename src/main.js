69a5a1d5d387f3997c75adf8863eea0e0e45ad5a
bin597246 <597246784@qq.com>
2020-11-29 13:58:39 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 
 Vue.config.productionTip = false
+Vue.use(VueAxios,axios)
+axios.defaults.baseURL = "http://119.3.104.39:2302/api"
+// console.log(this)
+axios.post('​/user_login​/Login',{
+  "login_account": "15110283003",
+  "login_password": "283003"
+}).then(res=>{
+  console.log(res)
+})
 
 new Vue({
   router,
